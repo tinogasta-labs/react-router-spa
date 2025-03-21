fc346dac7db01386d0bbd1ebbe145bae91bbe51f
roblesdotdev <httpsnip@gmail.com>
2025-03-21 18:08:45 -0300

    remove unused boilerplate

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    port: Number(process.env.PORT || 3000),
+  },
+  build: {
+    cssMinify: true,
+    minify: true,
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
